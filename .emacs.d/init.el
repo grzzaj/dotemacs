5ef6131436d4d3cbe9d8f9740a12735d7cdc6edc
jacekmigacz <jacekmigacz@icloud.com>
2013-05-25 18:28:28 +0200

    Initial commit.

---
+++ .emacs.d/init.el
@@ -0,0 +1,113 @@
+(add-to-list 'load-path (concat user-emacs-directory "/lisp"))
+
+(require 'exec-path-from-shell)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+(setq default-major-mode 'text-mode)
+(setq frame-title-format '(buffer-file-name "%f" ("%b")))
+
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(if (eq system-type 'darwin)
+    (progn
+      (setq mac-option-key-is-meta t)
+      (setq mac-right-option-modifier nil)))
+
+(setq make-backup-files nil)
+(setq version-control 'never)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq ring-bell-function 'ignore)
+
+(setq-default indent-tabs-mode nil)
+(setq c-default-style "k&r" c-basic-offset 4)
+
+(setq nxml-child-indent 4)
+
+(delete-selection-mode 1)
+(setq mouse-yank-at-point t)
+(setq mouse-wheel-scroll-amount '(1 ((shift))))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(tooltip-mode -1)
+(line-number-mode 1)
+(column-number-mode 1)
+(blink-cursor-mode 1)
+(show-paren-mode 1)
+
+(setq ediff-window-setup-function (quote ediff-setup-windows-plain))
+(setq ediff-split-window-function (quote split-window-horizontally))
+
+(defun compilation-hook()
+  (when (not (get-buffer-window "*compilation*"))
+    (save-selected-window
+      (save-excursion
+        (let* ((w (split-window-vertically))
+               (h (window-height w)))
+          (select-window w)
+          (switch-to-buffer "*compilation*"))))))
+(add-hook 'compilation-mode-hook 'compilation-hook)
+(setq compilation-scroll-output t)
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+(if (eq window-system 'x)
+    (progn
+      (set-frame-font "DejaVu Sans Mono-11")))
+(if (eq window-system 'ns)
+    (progn
+      (set-frame-font "Menlo-14")
+      (add-to-list 'default-frame-alist '(height . 50))
+      (add-to-list 'default-frame-alist '(width . 160))))
+
+(setq delete-by-moving-to-trash t)
+(if (eq window-system 'ns)
+    (progn
+      (setq trash-directory "~/.Trash/emacs")))
+
+(setq x-select-enable-clipboard t)
+
+(global-set-key (kbd "<M-down>") 'scroll-up-line)
+(global-set-key (kbd "<M-up>") 'scroll-down-line)
+(global-set-key (kbd "s-<left>") 'windmove-left)
+(global-set-key (kbd "S-s-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "s-<right>") 'windmove-right)
+(global-set-key (kbd "S-s-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "s-<up>") 'windmove-up)
+(global-set-key (kbd "S-s-<up>") 'enlarge-window)
+(global-set-key (kbd "s-<down>") 'windmove-down)
+(global-set-key (kbd "S-s-<down>") 'shrink-window)
+(global-set-key (kbd "C-l") 'goto-line)
+(global-set-key (kbd "C-k") 'kill-whole-line)
+(global-set-key (kbd "s-s") 'tags-search)
+(global-set-key (kbd "<M-f9>") 'compile)
+(global-set-key (kbd "<f9>") 'recompile)
+(global-set-key (kbd "M-]") 'next-error)
+(global-set-key (kbd "M-[") 'previous-error)
+(global-set-key (kbd "<f11>") 'toggle-fullscreen)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(require 'xcscope)
+(global-set-key (kbd "C-c s a") 'cscope-set-initial-directory)
+
+(require 'revbufs)
+
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(defun snippet-include (name)
+  (interactive "sEnter name of sentinel macro: ")
+  (insert (format "#ifndef %s\n#define %s\n\n#endif\n" name name)))
